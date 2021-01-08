dff02a09a6b26ea26b34210d99f98fa28b5f2704
Frantz680 <dubdub68250@hotmail.fr>
2021-01-08 10:23:23 +0100

    Ajout du css et jv

--- static/script.js
+++ static/script.js
@@ -1,33 +1,55 @@
+//-----------------Variable Global-----------------
+
+var text = ["SALUT", "BONJOUR", "HEY", "COUCOU"]
+var random = getRandomInt(0, text.length - 1)
+
+//-----------------Differentes Fonctions-----------------
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var text = ["SALUT", "BONJOUR", "HEY", "COUCOU"]
-var random = getRandomInt(0, text.length - 1)
 window.onload = function () {
-    document.getElementById("valeur_reponse_user").setAttribute("value", text[random]);
-}
+    document.getElementById("str_user").setAttribute("value", text[random]);
 
-function text_user() {
-    var reponse_user = document.getElementById("valeur_reponse_user").value
-    //alert(string_user.length)
-    if (reponse_user.length <= 3) {
-        document.getElementById("erreur").innerHTML = "Tapez autre chose"
+
+    var input = document.getElementById("str_user");
+
+    input.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            //alert(string_user.length)
+            if (input.value.length <= 3) {
+                document.getElementById("erreur").innerHTML = "Tapez autre chose"
+            }
+            else {
+                //Envoie de la requete
+                spinner.style.display = "block";
+                request_ajax(response_request_string_user)
+            }
+        }
+
+    });
+
+    function response_request_string_user() {
+
+        spinner.style.display = "none";
+        console.log(input.value);
     }
-    else {
-        //Envoie de la requete
-        spinner.style.display = "block";
 
+
+
+    function request_ajax(callback) {
+    //Fonction assynchrone
+    
         var xhttp = new XMLHttpRequest();
 
         xhttp.onreadystatechange = function () {
-            //Fonction assynchrone
+            
             if (this.readyState == 4 && this.status == 200) {
                 //var rep_json = JSON.parse(this.response);
-                spinner.style.display = "none";
-                console.log(reponse_user);
+                callback()
             }
         };
 
