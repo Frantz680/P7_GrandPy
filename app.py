5060fd2418f4a74e7c722234f41d54e5b34859e0
Frantz680 <dubdub68250@hotmail.fr>
2021-02-05 12:48:48 +0100

    V0.40

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+"""
+Website "GrandPy" Grandpa knows a lot.
+"""
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 import json
+
 from parse import Parse
-from api import Api
+from API.geolocalisation import Geolocalisation
+from API.wikipedia import Wikipedia
+
+"""
+Import different class.
+"""
 
 
 @app.route('/')
 def index():
-    return render_template('test.html' )#return envoie la reponse
+    return render_template('test.html')
+    # return envoie la reponse
 
-@app.route('/str_user', methods = ['POST'])
+
+@app.route('/str_user', methods=['POST'])
 def reponse_user():
     if request.method == 'POST':
         key_word = request.form['param_send']
@@ -26,85 +38,47 @@ def reponse_user():
                 if parse_word['api'] == 'True':
                     return api_envoi_reponse(parse_word)
 
-            else:   
+            else:
                 return parse_word
 
         elif parse_word['salutation'] == 'True':
-            
+
             if parse_word['api'] == 'True':
                 return api_envoi_reponse(parse_word)
 
             return parse_word
 
-        else:   
+        else:
             return parse_word
 
-def api_envoi_reponse(parse_word):            
+
+def api_envoi_reponse(parse_word):
+    """
+
+    :param parse_word:
+    :return:
+    """
+
     #print("parsseee", parse_word)
-    response_maps = json.loads(response_api_maps(parse_word['key_word']))
-    response_wiki = response_api_wiki(parse_word['key_word'])
+    response_maps = json.loads(api_geo.response_api_maps(parse_word['key_word']))
+    response_wiki = api_wiki.response_api_wiki(parse_word['key_word'])
     #print(response_maps + response_wiki)
     #On return du JSON
     #print(response_maps)
-    #print("ENVOImaps", response_maps, "ENVOImaps", response_wiki) 
+    #print("ENVOImaps", response_maps, "ENVOImaps", response_wiki)
     #print( json.dumps({"maps": response_maps, "wiki" : response_wiki}))
     #print(response_maps)
     #print(json.dumps({"adresse": response_maps["adresse_maps"], "location": response_maps["location_maps"],\
     #"wiki" : response_wiki, "insulte" : parse_word["insulte"]}))
     return json.dumps({"adresse": response_maps["adresse_maps"], "location": response_maps["location_maps"],\
-        "wiki" : response_wiki, "insulte" : parse_word["insulte"], "salutation" : parse_word["salutation"], "api" : parse_word["api"]})
+        "wiki": response_wiki, "insulte": parse_word["insulte"], "salutation": parse_word["salutation"],\
+                       "api": parse_word["api"]})
 
-
-
-def response_api_maps(parse_word):
-    params_maps = {
-        "address": str(parse_word),
-        "key": "AIzaSyChZyqQKTyt4r4GEXd9csdM7qUNXu5QT2A"
-    }
-
-    response_maps = maps.request_api(params_maps)
-    maps_adress = response_maps["results"][0]["formatted_address"]
-    maps_location = response_maps["results"][0]["geometry"]["location"]
-    #print("maps_adress", maps_adress)
-    #print("maps_location", maps_location)
-    return json.dumps({"adresse_maps": maps_adress, "location_maps" : maps_location})
-
-def response_api_wiki(parse_word):
-
-    params_wiki_location  = {
-        "action" : "query",
-        "namespace" : "0",
-        "list" : "search",
-        "srsearch" : str(parse_word),
-        "limit" : "10",
-        "format" : "json"
-    }
-    
-    reponse_wiki_location = wiki.request_api(params_wiki_location)
-
-    for page in reponse_wiki_location["query"]["search"]:
-        #print(page["pageid"])
-        page_wiki = page["pageid"]
-        break
-
-    params_wiki_page_wiki  = {
-        'action': "query",
-        'pageids': str(page_wiki),
-        'format': "json",
-        'prop': 'extracts',
-        'explaintext': 1,
-        'exsentences': 7
-    }
-    
-    response_wiki = wiki.request_api(params_wiki_page_wiki)
-    #print(wiki.request_api(params_wiki_page_wiki))
-    return  response_wiki["query"]["pages"][str(page_wiki)]["extract"]
 
 if __name__ == '__main__':
     parse_key_word = Parse()
 
-    wiki  =  Api ( "https://fr.wikipedia.org/w/api.php" )
-    maps  =  Api ( "https://maps.googleapis.com/maps/api/geocode/json" )
+    api_wiki = Wikipedia()
+    api_geo = Geolocalisation()
 
     app.run()
-    
--- parse.py
+++ parse.py
@@ -4,11 +4,21 @@ Bonsoir Grandpy, j'espère que tu as passé une belle semaine. Est-ce que tu pou
 Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"""
 
 import json
-import random
-   
+"""
+Import the module json.
+"""
+
+
 class Parse:
+    """
+    This class is used for
+    parse value to type user.
+    """
 
     def __init__(self):
+        """
+        We build the instance of the class.
+        """
 
         self.salutation = ["Salut", "Bonjour", "Hey", "Coucou", "Bonsoir", "Hello", "Hi"]
         self.verbe = ["suis", "as", "es", "est", "sommes", "etes", "êtes", "fait", "passé", "ai", "indiquer", "avons", "avez", "ont", "avoir", "etre", "être", "indique", "indiques", "indiquons", "indiquez", "indiquent", "voir", "vois", "voit", "voyons", "voyez", "voient", "penser", "pense", "penses", "pensons", "pensez", "pensent", "pouvoir", "peux", "peut", "pouvons", "pouvez", "peuvent", "pourrais", "pourras", "pourra", "pourrons", "pourrez", "pourront", "trouver", "trouve", "trouves", "trouvons", "trouvez", "trouvent", "connaitre", "connaître", "connais", "connait", "connaît", "connaissons", "connaissez", "connaissent", "grandpy", "grandpy", "grandpy", "grandpy", "grandpy", "grandpy", "grandpy",]
@@ -30,60 +40,64 @@ class Parse:
         self.status_insulte = {'insulte': 'False'}
         self.status_api = {'api': 'False'}
         self.status = {}
-        self.random = random.choice(self.salutation)
 
-        #---------On additionne tous les tableaux ensembles pour le parse ---------
+        """---------On additionne tous les tableaux ensembles pour le parse ---------"""
 
         self.tbl_remove_word = self.verbe + self.pronom + self.mot + self.ponctuation + self.alphabet
 
     def parse_str_user(self, value_input):
+        """
+        On parse la valeur entre par l'utilisateur pour recuperer les mots clés.
+        :param value_input: Valeur du input de l'utilisateur.
+        :return: Les mots clés.
+        """
 
         self.status_salutation = {'salutation': 'False'}
         self.status_insulte = {'insulte': 'False'}
         self.status_api = {'api': 'False'}
 
-        for index in range (0, len(self.salutation), 1):
+        for index in range(0, len(self.salutation), 1):
             self.salutation_mini.append(self.salutation[index].lower())
 
         print(value_input)
         print(self.key_word)
 
-        #On met tout les charactere en minuscules
+        """On met tout les charactere en minuscules"""
         value_input = self.transform_mini(value_input)
             
-        #On supprime les ponctuations
+        """On supprime les ponctuations"""
         value_input = self.remove_ponctuation(value_input)
 
-        #On crée un tableau
-        self.key_word = self.create_tableau(value_input)
-        print("keyword tableau", self.key_word)
+        """On crée un tableau"""
+        value_input_tbl = self.create_tableau(value_input)
+        print("keyword tableau", len(value_input_tbl))
 
-        #On regarde dans la chaine si il y a un mot de salutation, si oui on returne un True
-        self.status_salutation = self.parse_salutation(self.key_word)
+        """On regarde dans la chaine si il y a un mot de salutation, si oui on returne un True"""
+        self.status_salutation = self.parse_salutation(value_input_tbl)
 
-        #On supprime les mots de salutation
-        self.key_word = self.remove_word(self.key_word, self.salutation_mini)
+        """On supprime les mots de salutation"""
+        self.key_word = self.remove_word(value_input_tbl, self.salutation_mini)
 
-        #On regarde dans la chaine si il y a un mot d'insulte, si oui on returne un True
+        """On regarde dans la chaine si il y a un mot d'insulte, si oui on returne un True"""
         self.status_insulte = self.parse_mot_insulte(self.key_word)
 
-        #On supprime les mots d'insultes
+        """On supprime les mots d'insultes"""
         self.key_word = self.remove_word(self.key_word, self.insulte)
 
-        #On supprime les mots restants
+        """On supprime les mots restants"""
         self.key_word = self.remove_word(self.key_word, self.tbl_remove_word)
 
-        #On additionne les status salutation et insulte ensemble
+        """On additionne les status salutation et insulte ensemble"""
         self.status.update(self.status_salutation)
         self.status.update(self.status_insulte)
         print(self.status)
 
         print(self.key_word)
 
-        #On regarde dans la chaine si il y a plus que un mot, si oui on return un True
-        self.status_api = self.parse_api(self.key_word)
+        """On regarde dans la chaine si il y a plus que un mot, si oui on return un True"""
+        self.status_api = self.parse_api(value_input_tbl)
 
-        #On ajoute le status api aux autres status
+        """On ajoute le status api aux autres status"""
         self.status.update(self.status_api)
 
         print(self.status)
@@ -91,16 +105,20 @@ class Parse:
         return json.dumps(self.status)
     
     def parse_api(self, param):
+        """
+        Permet de savoir si on envoi les mots clé l'api
+        :param param: Valeur du input de l'utilisateur
+        :return: status de l'api True ou False
+        """
         
-
-        if (len(param) < 2):
-            print("LONGUEUR",len(param))
+        if len(param) < 2:
+            print("LONGUEUR", len(param))
             self.status_api = {'api': 'False'}
 
-            #On converti les mots clés en dictionnaire
+            """On converti les mots clés en dictionnaire"""
             self.key_word = dict({'key_word': 'Nothing'})
 
-            #On ajoute les status au mots clés
+            """On ajoute les status au mots clés"""
             self.status.update(self.key_word) 
             return self.status_api
             
@@ -108,16 +126,19 @@ class Parse:
             print("LONGUEUR",len(param))
             self.status_api = {'api': 'True'}
 
-            #On converti les mots clés en dictionnaire
+            """On converti les mots clés en dictionnaire"""
             self.key_word = dict({'key_word': self.key_word})
 
-            #On ajoute les status au mots clés
+            """On ajoute les status au mots clés"""
             self.status.update(self.key_word) 
             return self.status_api
-            
 
     def parse_salutation(self, param):
-    # si un mot de saluation est présent dans la pharse le bot salut
+        """
+        Permet de savoir si le papy va dire bonjour.
+        :param param: Valeur du input de l'utilisateur
+        :return: status de salutation True ou False
+        """
 
         for index in range (0, len(self.salutation_mini), 1):
             for index_param in range (0, len(param), 1):
@@ -127,10 +148,13 @@ class Parse:
                     return self.status_salutation
 
         return self.status_salutation
-                
 
     def parse_mot_insulte(self, param):
-    # si un mot interdit est présent dans la pharse la maman repond
+        """
+        Permet de savoir si la maman va dire quelque chose ou non.
+        :param param: Valeur du input de l'utilisateur.
+        :return: status de insulte True ou False.
+        """
 
         for index in range (0, len(self.insulte), 1):
             for index_param in range (0, len(param), 1):
@@ -143,44 +167,54 @@ class Parse:
         return self.status_insulte
         
     def transform_mini(self, param):
-    # on met toute les char en minuscule
+        """
+        Permet de mettre tout les char en minuscule.
+        :param param: Valeur du input de l'utilisateur.
+        :return: Valeur du input en minuscule de l'utilisateur.
+        """
+
         return param.lower()
 
     def remove_ponctuation(self, param):
-    # on remplace la ponctuation par un espace 
+        """
+        Permet de remplacer la ponctuation par un espace.
+        :param param: Valeur du input de l'utilisateur.
+        :return: Valeur du input sans ponctuation de l'utilisateur.
+        """
 
-        for index in range (0,len(self.ponctuation),1):
+        for index in range(0, len(self.ponctuation), 1):
 
             param = param.replace(self.ponctuation[index], " ")
     
         return param
 
-    def create_tableau(self, param): 
-        # on créer un tableau avec notre chaine de caractere
+    def create_tableau(self, param):
+        """
+        On créer un tableau avec notre chaine de charactere de l'utilisateur.
+        :param param: Valeur du input de l'utilisateur.
+        :return: Valeur du input dans un tableau.
+        """
+
         return param.split(" ")
 
-
     def remove_word(self, p_key_word, p_remove_word):
-        # On remplace(supprimer) tout les mots inutiles dans notre tableau 
-        for index in range (0,len(p_key_word),1):
-            for index_remove in range (0,len(p_remove_word),1): 
-                if (p_key_word[index] == p_remove_word[index_remove]):
+        """
+        On remplace tout les mots inutiles par des ""
+        :param p_key_word: Valeur du input de l'utilisateur.
+        :param p_remove_word: Valeur des mots à supprimer.
+        :return: La valeur avec que les mots clés.
+        """
+
+        for index in range(0, len(p_key_word), 1):
+            for index_remove in range(0, len(p_remove_word), 1):
+                if p_key_word[index] == p_remove_word[index_remove]:
                     p_key_word[index] = p_key_word[index].replace(p_remove_word[index_remove], "")
         
-        #print("p_key_word1", p_key_word )
-        
-        #print("p_key_word4444")
-
-        # On ajoute les mots dans un nouveau tableau mais pas les "" 
+        'On ajoute les mots dans un nouveau tableau mais pas les "" '
         new_input_value = []
-        for index in range (0,len(p_key_word),1): 
-            if (p_key_word[index] != ""):
+        for index in range(0, len(p_key_word), 1):
+            if p_key_word[index] != "":
                 #print(p_key_word)
                 new_input_value.append(p_key_word[index])
         
         return new_input_value
-
-
-    #def response_server(self, response):
-
-        #self.print_papy("envoi serveur " + str(response))
